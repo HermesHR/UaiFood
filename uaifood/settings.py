eef140c8985bfd0187875a594aced72cc7e608c2
HermesHR <59778974+HermesHR@users.noreply.github.com>
2021-12-04 21:06:43 -0300

    UPDATE

--- uaifood/settings.py
+++ uaifood/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'uaifood',
         'PORT': 5432,
-        'USER': 'postgres',
-        'PASSWORD': 'artilharia',
-        'HOST': 'localhost'
+        'USER': 'hermes',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1'
     }
 }
 
